d97fd61fc45410d53bafad72b4cb716ce65e8eac
Thangadurai Nainamalai <duraithanga3@gmail.com>
2020-12-25 22:18:01 +0530

    Update sidemenu design

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,48 +1,29 @@
-import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import * as AiIcons from 'react-icons/ai';
-import * as FaIcons from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import { IconContext } from 'react-icons';
 
-const Sidebar = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar);
-
-  return (
-    <IconContext.Provider value={ { color: '#fff' } }>
-      {
-        sidebar && (
-          <nav className="nav-menu">
-            <ul className="nav-menu-items" onClick={showSidebar}>
-              <li className="navbar-toggle">
-                <Link to="#" className="menu-bars">
-                  <AiIcons.AiOutlineClose />
-                </Link>
+const Sidebar = () => (
+  <IconContext.Provider value={ { color: '#fff' } }>
+    {
+      <nav className="nav-menu">
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            Profile portfolio
+          </li>
+          {
+            SidebarData.map((item, index) => (
+              <li key={index} className={item.className}>
+                <NavLink activeClassName="active" exact replace to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </NavLink>
               </li>
-              {
-                SidebarData.map((item, index) => (
-                  <li key={index} className={item.className}>
-                    <NavLink activeClassName="active" exact replace to={item.path}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </NavLink>
-                  </li>
-                ))
-              }
-            </ul>
-          </nav>
-        ) || (
-          <div className="navbar">
-            <Link to='#' className="menu-bars">
-              <FaIcons.FaBars onClick={showSidebar} />
-            </Link>
-          </div>
-        )
-      }
-    </IconContext.Provider>
-  )
-};
+            ))
+          }
+        </ul>
+      </nav>
+    }
+  </IconContext.Provider>
+);
 
 export default Sidebar;
