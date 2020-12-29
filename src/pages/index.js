e93c212ee9997b24e844a7259ef66559d28807c7
Thangadurai Nainamalai <duraithanga3@gmail.com>
2020-12-29 23:58:04 +0530

    Added page spinner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,38 @@
+import { useEffect } from 'react';
 import { Footer, Header, Sidebar } from 'Components';
 import ReactRouter5 from './Router';
 
-const Pages = () => (
-  <div className="container">
-    <section className="container-left">
-      <Sidebar />
-    </section>
-    <section className="container-right">
-      <Header />
-      <div className="content-area">
-        <ReactRouter5 />
+const Pages = () => {
+  const loader = true;
+
+  if (loader) {
+    return (
+      <div className="container loading">
+        <section className="container-right">
+          <Header />
+          <div className="content-area">
+            <img src="/assets/images/spinner.gif" alt="page spinner" />
+          </div>
+          <Footer />
+        </section>
       </div>
-      <Footer />
-    </section>
-  </div>
-);
+    )
+  }
+
+  return (
+    <div className="container">
+      <section className="container-left">
+        <Sidebar />
+      </section>
+      <section className="container-right">
+        <Header />
+        <div className="content-area">
+          <ReactRouter5 />
+        </div>
+        <Footer />
+      </section>
+    </div>
+  );
+};
 
 export default Pages;
